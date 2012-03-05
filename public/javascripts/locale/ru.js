bb82d10a9baa1a10d75ab512cdbccea6d68eb050
Kirill Jakovlev <special-k@li.ru>
2012-03-05 14:45:39 +0400

    locale

---
+++ public/javascripts/locale/ru.js
@@ -0,0 +1,32 @@
+
+gxp.plugins.ViewMenu.prototype.panelText = 'Вид';
+
+gxp.plugins.ViewMenu.prototype.panelTooltip = 'Вид';
+
+gxp.plugins.ViewMenu.prototype.graticulText = 'Координатная сетка';
+
+gxp.plugins.ViewMenu.prototype.graticulTooltip = 'Координатная сетка';
+
+gxp.plugins.ViewMenu.prototype.projectionsGroupText = 'Проекции';
+
+gxp.plugins.ViewMenu.prototype.projectionsGroupTooltip = 'Проекции';
+
+gxp.plugins.ViewMenu.prototype.overviewMapText = 'Обзорная карта';
+
+gxp.plugins.ViewMenu.prototype.overviewMapTooltip = 'Обзорная карта';
+
+gxp.plugins.ViewMenu.prototype.rssText = 'Rss';
+
+gxp.plugins.ViewMenu.prototype.rssTooltip = 'Rss';
+
+gxp.plugins.ViewMenu.prototype.wrapText = 'Неразрывная карта';
+
+gxp.plugins.ViewMenu.prototype.wrapTooltip = 'Неразрывная карта';
+
+gxp.plugins.ViewMenu.prototype.projectionsText = {
+  'EPSG:900913': 'Меркатор',
+  'EPSG:4326': 'Географическая',
+  'EPSG:102012': 'Коническая',
+  'EPSG:3576': 'Полярная сервер',
+  'EPSG:3976': 'Полярная юг'
+};
